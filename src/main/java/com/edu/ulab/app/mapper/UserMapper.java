27fab19151388ed0742ee7a1474c1c27de297331
Plinio <avzhykov@gmail.com>
2022-09-18 22:27:29 +0600

    Создано хранилище, добавлены необходимые сущности, обновлены мапперы (UserMapper.java и BookMapper.java), обновлены методы сервисов и UserDataFacade.java.

--- src/main/java/com/edu/ulab/app/mapper/UserMapper.java
+++ src/main/java/com/edu/ulab/app/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.edu.ulab.app.mapper;
 
 import com.edu.ulab.app.dto.UserDto;
+import com.edu.ulab.app.entity.User;
+import com.edu.ulab.app.web.request.UserBookRequest;
 import com.edu.ulab.app.web.request.UserRequest;
 import org.mapstruct.Mapper;
 
@@ -9,4 +11,8 @@ public interface UserMapper {
     UserDto userRequestToUserDto(UserRequest userRequest);
 
     UserRequest userDtoToUserRequest(UserDto userDto);
+
+    UserDto userToUserDto(User user);
+
+    User userDtoToUser(UserDto userDto);
 }
--- src/main/java/com/edu/ulab/app/storage/Storage.java
+++ src/main/java/com/edu/ulab/app/storage/Storage.java
@@ -1,11 +1,65 @@
 package com.edu.ulab.app.storage;
 
-public class Storage {
-    //todo создать хранилище в котором будут содержаться данные
-    // сделать абстракции через которые можно будет производить операции с хранилищем
-    // продумать логику поиска и сохранения
-    // продумать возможные ошибки
-    // учесть, что при сохранеии юзера или книги, должен генерироваться идентификатор
-    // продумать что у узера может быть много книг и нужно создать эту связь
-    // так же учесть, что методы хранилища принимают друго тип данных - учесть это в абстракции
+import com.edu.ulab.app.entity.Book;
+import com.edu.ulab.app.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Storage {
+
+    /**
+     * Получение пользователя
+     * @param id ID пользователя
+     * @return Optional<User> пользователь с данным id
+     */
+    Optional<User> getUserById(Long id);
+
+    /**
+     * Обновление пользователя
+     * @param user пользователь
+     * @return Optional<User> пользователь
+     */
+    Optional<User> updateUser(User user);
+
+    /**
+     * Сохранение пользователя
+     * @param user пользователь
+     * @return Optional<User> пользователь
+     */
+    Optional<User> saveUser(User user);
+
+    /**
+     * Удаление пользователя
+     * @param id ID пользователя
+     */
+    boolean deleteUser(Long id);
+
+    /**
+     * Получение книги по id
+     * @param id ID книги
+     * @return Optional<Book> книга
+     */
+    Optional<Book> getBookById(Long id);
+
+    /**
+     * Сохранение книги
+     * @param book книга
+     * @return обновленная книга
+     */
+    Optional<Book> updateBook(Book book);
+
+    /**
+     * Сохранение книги
+     * @param book книга
+     * @return либо обновленная, либо новая книга
+     */
+    Optional<Book> saveBook(Book book);
+
+    /**
+     * Удаление книги
+     * @param id ID книги
+     */
+    boolean deleteBook(Long id);
+
 }
